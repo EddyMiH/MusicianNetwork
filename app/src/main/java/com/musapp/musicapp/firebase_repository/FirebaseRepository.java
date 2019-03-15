58baf79b360b357ba589a9bd1764c08516bb32a9
goshACA <kinjiyan.gohar@gmail.com>
2019-03-15 21:00:12 +0400

    add get posts and genres

--- app/src/main/java/com/musapp/musicapp/firebase_repository/FirebaseRepository.java
+++ app/src/main/java/com/musapp/musicapp/firebase_repository/FirebaseRepository.java
@@ -6,8 +6,7 @@ import com.musapp.musicapp.currentinformation.CurrentUser;
 import com.musapp.musicapp.firebase.DBAccess;
 
 public class FirebaseRepository {
-    public static void getGenres(ValueEventListener valueEventListener) {
-    }
+
 
     public static void setUserInnerPrimaryKey(ValueEventListener valueEventListener) {
         Query lastQuery = DBAccess.getDatabaseReference().child("users").orderByKey().limitToLast(1);
@@ -22,14 +21,25 @@ public class FirebaseRepository {
     public static void setUserInnerPrimaryKeyToFirebase() {
         DBAccess.createField(CurrentUser.getCurrentUser().getPrimaryKey(), "users/" + CurrentUser.getCurrentUser().getPrimaryKey() + "/primaryKey");
     }
-    public static void setUserHashedPassword(String password){
+
+    public static void setUserHashedPassword(String password) {
         DBAccess.createField(password, "users/" + CurrentUser.getCurrentUser().getPrimaryKey() + "/password");
 
     }
 
 
-    public static void getUser(ValueEventListener valueEventListener){
+    public static void getUser(ValueEventListener valueEventListener) {
         DBAccess.getUserReference("users").addValueEventListener(valueEventListener);
+    }
+
+    public static void getPosts(String userPrimaryKey, int limit, int offset, ValueEventListener valueEventListener) {
+       Query postQuery = DBAccess.getDatabaseReference().child("posts").orderByChild("userId").equalTo(userPrimaryKey);
+       postQuery.addValueEventListener(valueEventListener);
+    }
+
+    public static void getGenres(String userPrimaryKey, ValueEventListener valueEventListener){
+        Query genreQuery = DBAccess.getUserReference("users/" + userPrimaryKey).child("genreId");
+        genreQuery.addListenerForSingleValueEvent(valueEventListener);
     }
 
 }
