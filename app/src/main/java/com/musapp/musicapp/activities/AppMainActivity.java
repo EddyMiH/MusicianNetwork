eb5d11c0c7f33edca0bb664d1f67057eb99859b9
EddyMiH <45766975+EddyMiH@users.noreply.github.com>
2019-04-02 01:13:24 +0400

    add video full screen and playing speed changes features

--- app/src/main/java/com/musapp/musicapp/activities/AppMainActivity.java
+++ app/src/main/java/com/musapp/musicapp/activities/AppMainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -21,6 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -317,6 +319,18 @@ public class AppMainActivity extends AppCompatActivity {
         void userImageClickListener(Post post);
         void postClickListener(Post post);
         void userCommentImageClickListener(String userId);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Toast.makeText(this, "orientation changes", Toast.LENGTH_SHORT).show();
+            mToolBarTitle.hideNavigationBar();
+        }else{
+            mToolBarTitle.showNavigationBar();
+        }
     }
 
     @Override
